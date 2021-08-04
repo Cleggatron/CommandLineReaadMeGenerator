b9e0b597731798c9b3cdff2c08cf6c00366f044c
Cleggatron <david.robert.clegg@gmail.com>
2021-08-04 21:37:04 +0100

    Created dependancies and a test question.

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const util = require("util");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./generateMarkdown.js");
+
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const promptUser = () => {
+    return inquirer.prompt([
+    {
+        type: "input",
+        message: "What is your project title?",
+        name: "title"
+    } 
+])};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
