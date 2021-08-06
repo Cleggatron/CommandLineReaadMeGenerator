aef29428b7cc4c42f51a451df1bf601b5bd19198
Cleggatron <david.robert.clegg@gmail.com>
2021-08-06 17:00:05 +0100

    created the general markdown structure and tested

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -12,7 +12,56 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  console.log(data)
+  let licenseBadge = "test";
   return `# ${data.title}
+
+  https://github.com/${data.username}/${data.title}
+  
+  ${licenseBadge}
+
+  ## Description
+
+  ${data.description}
+
+  ## Table Of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  - [Questions](#questions)
+
+  ## Installation
+
+  ${data.installation}
+
+  ## Usage
+
+  ![File In Action](${data.usageScreenshot})
+
+  ${data.usage}
+
+  ## Credits
+
+  Other Contributors: 
+  ${data.contributors}
+
+  Third Party Technologies:
+  ${data.technology}
+
+  ## License
+
+  ${data.license}
+
+  ## Questions
+
+  If you have any questions you can contact me at the below email address:
+  ${data.emailAddress}
+
+  You can also find me at my Github:
+  https://github.com/${data.username}
+
 
 `;
 }
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const util = require("util");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./generateMarkdown.js");
 
+//Creates our readme asynchronously
 const writeFileAsync = util.promisify(fs.writeFile);
 
 // TODO: Create an array of questions for user input
@@ -11,13 +12,63 @@ const promptUser = () => {
     return inquirer.prompt([
     {
         type: "input",
-        message: "What is your project title?",
+        message: "Please enter your project title as it appears on Github",
         name: "title"
-    } 
+    }, 
+    {
+        type: "input",
+        message: "Please enter a short description of your project. Points to cover: What was your motivation?\nWhy did you build this?\nWhat does it solve?\nWhat did you learn?",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Please provide a step by step guide to the installation of the project.",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "Please provide the instructions and examples for use.",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "Please provide the relative file path for a screenshot.",
+        name: "usageScreenshot"
+    },
+    {
+        type: "input",
+        message: "Please list any contributors on this project as a comma seperated list (e.g Tom, Dick, Harry)",
+        name: "contributors"
+    },
+    {
+        type: "input",
+        message: "Please list any 3rd party technologies used as a comma seperated list (e.g JQuery, Moment.js, Bootstrap)",
+        name: "technology"
+    },
+    {
+        type: "list",
+        message: "Please pick the license you wish to apply.",
+        name: "license",
+        choices : ["GNU Affero General Public License v3.0", "Apache License 2.0", "BSD 2-Clause \"Simplified\" License", "BSD 3-Clause \"New\" or \"Revised\" License", "Boost Software License 1.0", "Creative Commons Zero v1.0 Universal", "Eclipse Public License 2.0", "GNU General Public License v2.0", "GNU General Public License v3.0", "GNU Lesser General Public License v2.1", "MIT License", "Mozilla Public License 2.0", "The Unlicense", "None"]       
+    },
+    {
+        type: "input",
+        message: "Please enter your Github Username",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "Please input your email address.",
+        name: "emailAddress"
+    }
+    // {
+    //     type: "input",
+    //     message: "Please input any tests for the code.",
+    //     name: "tests"
+    // }
+
 ])};
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {
